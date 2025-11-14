a46f2ad873cb8675b25f02e388b217a6c41d23e5
david-vizena <davidvizena@gmail.com>
2025-11-14 11:03:35 -0600

    Fix OpenTelemetry Resource import issue in js-gateway

--- applications/js-gateway/tracing.js
+++ applications/js-gateway/tracing.js
@@ -1,6 +1,7 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const opentelemetry = require('@opentelemetry/api');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
